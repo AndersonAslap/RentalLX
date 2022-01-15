428f14668a491025a77875225a2b49e468dd54a2
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-01-14 23:56:50 -0300

    Criaçao da seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,17 @@
+import { hash } from "bcrypt";
+import { createConnection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidv4();
+  const password = hash("admin", 8);
+
+  await connection.query(
+    `insert into users(id, name, email, password, "isAdmin", created_at, driver_license)
+      values(${id}, 'admin', 'admin@rentx.com.br', ${password}, true, NOW(), 'xxxxxxxx'}`
+  );
+}
+
+create().then(() => console.log("user admin created!"));
