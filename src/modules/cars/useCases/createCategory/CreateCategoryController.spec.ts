d8302fdf6c31470e237586ed05fcff3a1b219c6e
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-02-05 17:33:10 -0300

    Corrigindo os testes

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -7,7 +7,7 @@ import { app } from "@shared/infra/http/app";
 import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
-let token: string;
+let refresh_token: string;
 
 describe("Create Category Controller", () => {
   beforeAll(async () => {
@@ -27,7 +27,7 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    token = responseToken.body.token;
+    refresh_token = responseToken.body.refresh_token;
   });
 
   afterAll(async () => {
@@ -43,7 +43,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     expect(response.status).toBe(201);
@@ -57,7 +57,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     expect(response.status).toBe(400);
