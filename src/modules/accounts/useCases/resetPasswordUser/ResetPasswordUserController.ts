fca33c43edd127a42ce571c67af4861491716d60
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-02-05 17:00:06 -0300

    Finalizando Modulo de reset de senha

--- src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -5,9 +5,15 @@ import { ResetPasswordUserUseCase } from "./ResetPasswordUserUseCase";
 
 class ResetPasswordUserController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { token } = request.query;
+    const { password } = request.body;
+
     const resetPasswordUserUseCase = container.resolve(
       ResetPasswordUserUseCase
     );
+
+    await resetPasswordUserUseCase.execute({ token: String(token), password });
+
     return response.send();
   }
 }
